481ac93b65531e53e2757142b80d36cd4f3feeab
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-05-26 08:05:20 +0200

    jnh

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -33,32 +33,3 @@ router.post('/', async (req, res) => {
 });
 
 export default router;
-{/*
-import express from 'express';
-import Order from '../models/Order.js';
-import CartItem from '../models/CartItem.js';
-import { sendClientConfirmation, sendAdminNotification } from '../utils/mailer.js';
-
-const router = express.Router();
-
-router.post('/', async (req, res) => {
-  try {
-    const order = req.body;
-    console.log('📦 Отримано замовлення:', order);
-
-    const savedOrder = await Order.create(order);
-    console.log('✅ Замовлення збережено:', savedOrder);
-
-    // ⬇️ Надіслати листи
-    await sendClientConfirmation(order);
-    await sendAdminNotification(order);
-
-    res.status(201).json(savedOrder);
-  } catch (error) {
-    console.error('❌ Помилка збереження замовлення:', error);
-    res.status(500).json({ error: 'Помилка при збереженні замовлення' });
-  }
-});
-
-export default router;
-*/}
